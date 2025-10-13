c012eed384eb73c29ee12da91e1aa74864bb798a
Chamod Mullegama <149089781+ChamodMullegama@users.noreply.github.com>
2025-10-13 20:01:46 +0530

    Update getAllUser.tsx

--- next02/lib/getAllUser.tsx
+++ next02/lib/getAllUser.tsx
@@ -6,8 +6,4 @@ export default async function getAllUser() {
 const res = await fetch('https://jsonplaceholder.typicode.com/users')
 if(!res.ok) throw new Error('Faild to fatch data')
   return res.json()
-
-
-
-  
 }
