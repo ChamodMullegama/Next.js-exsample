5c7f466fb42eb5dd56438855b0c1ba2c2a30cd3a
Chamod Mullegama <149089781+ChamodMullegama@users.noreply.github.com>
2025-10-17 22:35:43 +0530

    Update page.tsx

--- next02/app/page.tsx
+++ next02/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <main className={styles.main}>
              <h1>home page</h
 
-
+ 
                             
           <Link href="/users">users</Link>     </main
     </div>
