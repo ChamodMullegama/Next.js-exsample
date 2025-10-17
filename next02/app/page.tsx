eb76ed4e54c45ad6ab6f39e405a33ec9c0263571
Chamod Mullegama <149089781+ChamodMullegama@users.noreply.github.com>
2025-10-17 22:35:59 +0530

    Update page.tsx

--- next02/app/page.tsx
+++ next02/app/page.tsx
@@ -1,15 +1,1 @@
-import Link from "next/link";
-import styles from "./page.module.css";
 
-export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-             <h1>home page</h
-
-    
-                            
-          <Link href="/users">users</Link>     </main
-    </div>
-  );
-}
